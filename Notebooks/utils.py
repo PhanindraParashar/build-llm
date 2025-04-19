d7647f9a54b51340b9150859eb0060d0acfa8e40
PhanindraParashar49 <parasharphanindra@gmail.com>
2025-04-20 01:13:57 +0200

    fix indentation

--- Notebooks/utils.py
+++ Notebooks/utils.py
@@ -83,43 +83,43 @@ class CausalAttention(nn.Module):
         attention_weights = self.dropout(attention_weights)
         return attention_weights @ value # context vector
     
-    class MultiHeadAttention(nn.Module):
-        def __init__(self, d_in, d_out, num_heads):
-            super(MultiHeadAttention, self).__init__()
-            assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
-            self.num_heads = num_heads
-            self.head_dim = d_out // num_heads
-            self.values_w = nn.Linear(d_in, d_out)
-            self.keys_w = nn.Linear(d_in, d_out)
-            self.queries_w = nn.Linear(d_in, d_out)
-            self.fc_out = nn.Linear(d_out, d_out)
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.values_w = nn.Linear(d_in, d_out)
+        self.keys_w = nn.Linear(d_in, d_out)
+        self.queries_w = nn.Linear(d_in, d_out)
+        self.fc_out = nn.Linear(d_out, d_out)
 
-        def forward(self, x):
-            batch_size, input_length, _ = x.shape
+    def forward(self, x):
+        batch_size, input_length, _ = x.shape
 
-            values = self.values_w(x)
-            keys = self.keys_w(x)
-            query = self.queries_w(x)
+        values = self.values_w(x)
+        keys = self.keys_w(x)
+        query = self.queries_w(x)
 
-            # (batch_size, sequence_length, d_out) -> (batch_size, sequence_length, num_heads, head_dim)
-            keys = keys.view(batch_size, input_length, self.num_heads, self.head_dim)
-            query = query.view(batch_size, input_length, self.num_heads, self.head_dim)
-            values = values.view(batch_size, input_length, self.num_heads, self.head_dim)
+        # (batch_size, sequence_length, d_out) -> (batch_size, sequence_length, num_heads, head_dim)
+        keys = keys.view(batch_size, input_length, self.num_heads, self.head_dim)
+        query = query.view(batch_size, input_length, self.num_heads, self.head_dim)
+        values = values.view(batch_size, input_length, self.num_heads, self.head_dim)
 
 
-            # (batch_size, sequence_length, num_heads, head_dim) -> (batch_size, num_heads, sequence_length, head_dim)
-            keys = keys.transpose(1, 2)
-            query = query.transpose(1, 2)
-            values = values.transpose(1, 2)
+        # (batch_size, sequence_length, num_heads, head_dim) -> (batch_size, num_heads, sequence_length, head_dim)
+        keys = keys.transpose(1, 2)
+        query = query.transpose(1, 2)
+        values = values.transpose(1, 2)
 
-            attention_scores = query @ keys.transpose(2,3)/(self.head_dim**0.5)
-            mask = torch.triu(torch.ones(input_length, input_length), diagonal=1).bool()
-            attention_scores = attention_scores.masked_fill(mask, float('-inf'))
+        attention_scores = query @ keys.transpose(2,3)/(self.head_dim**0.5)
+        mask = torch.triu(torch.ones(input_length, input_length), diagonal=1).bool()
+        attention_scores = attention_scores.masked_fill(mask, float('-inf'))
 
-            attention_weights = torch.nn.functional.softmax(attention_scores, dim=-1)
-            attention_output = attention_weights @ values # batch_size x num_heads x sequence_length x head_dim
-            attention_output = attention_output.transpose(1, 2) # batch_size x sequence_length x num_heads x head_dim
-            
-            context_vector = attention_output.contiguous().view(batch_size, input_length, self.num_heads * self.head_dim) # batch_size x sequence_length x d_out
-            return self.fc_out(context_vector)
-            
+        attention_weights = torch.nn.functional.softmax(attention_scores, dim=-1)
+        attention_output = attention_weights @ values # batch_size x num_heads x sequence_length x head_dim
+        attention_output = attention_output.transpose(1, 2) # batch_size x sequence_length x num_heads x head_dim
+        
+        context_vector = attention_output.contiguous().view(batch_size, input_length, self.num_heads * self.head_dim) # batch_size x sequence_length x d_out
+        return self.fc_out(context_vector)
+        
